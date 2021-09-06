460f71b444122e1bd576a1870a0e758ad94ca986
Chris-AM <c.aranguizm@outlook.com>
2021-09-06 00:49:45 -0300

    now doesnt ruturn password when user created

--- models/user.model.js
+++ models/user.model.js
@@ -35,7 +35,7 @@ const UserSchema = Schema({
 });
 
 UserSchema.method('toJSON', function(){
-    const {__v, _id, ...object} = this.toObject();
+    const {__v, _id, password, ...object} = this.toObject();
     object.userID = _id;
     return object;
 })
