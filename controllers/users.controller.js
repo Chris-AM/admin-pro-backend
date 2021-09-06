a19158673cc04c1ac2ec550b25b86086b8e3cd65
Chris-AM <c.aranguizm@outlook.com>
2021-09-05 22:31:53 -0300

    creating post and get methods

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,10 +1,37 @@
-const getUsers =  (req, res) => {
+const User = require('../models/user.model');
+
+
+const getUsers = async (req, res) => {
+    const users = await User.find({}, 'name lastName email');
+
     res.json({
         ok: true,
-        users: []
+        msg: 'getting users',
+        users
+    });
+}
+
+const createUser = async (req, res) => {
+    const body = req.body;
+    const user = new User({
+        name: body.name,
+        lastName: body.lastName,
+        email: body.email,
+        password: body.password,
+        img: body.image,
+        role: body.role
+    });
+    console.log('user created', user);
+    await user.save();
+    
+    res.json({
+        ok: true,
+        msg: 'creating user',
+        user
     });
 }
 
 module.exports = {
     getUsers,
+    createUser
 }
