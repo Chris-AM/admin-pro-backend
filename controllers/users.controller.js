c9c8751c6516f200ff2f1ce11555bfccb4904f57
Chris-AM <c.aranguizm@outlook.com>
2021-09-05 23:43:03 -0300

    validating that email is already created or not

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const User = require('../models/user.model');
 
 
@@ -11,24 +12,37 @@ const getUsers = async (req, res) => {
     });
 }
 
-const createUser = async (req, res) => {
-    const body = req.body;
-    const user = new User({
-        name: body.name,
-        lastName: body.lastName,
-        email: body.email,
-        password: body.password,
-        img: body.image,
-        role: body.role
-    });
-    console.log('user created', user);
-    await user.save();
-    
-    res.json({
-        ok: true,
-        msg: 'creating user',
-        user
-    });
+const createUser = async (req, res = response) => {
+    const { name, lastName, email, password } = req.body;
+
+    try {
+        const emailExists = await User.findOne({email});
+        
+        if(emailExists){
+            return res.status(400).json({
+                ok: false,
+                msg: 'email already taken'
+            });
+        }
+
+        const user = new User(req.body);
+        console.log('user created', user);
+        await user.save();
+
+
+        res.json({
+            ok: true,
+            msg: 'creating user',
+            user
+        });
+
+    } catch (error) {
+        console.log('error --->', error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Internal server Error. Check Logs'
+        })
+    }
 }
 
 module.exports = {
