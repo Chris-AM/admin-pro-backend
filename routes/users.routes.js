2171b52bbc16095e73306f61a2f692bff23910eb
Chris-AM <c.aranguizm@outlook.com>
2021-09-05 22:33:05 -0300

    importing createUser method from controller

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,10 +3,11 @@
  */
 
 const {Router} = require('express');
-const {getUsers} = require('../controllers/users.controller')
+const {getUsers, createUser} = require('../controllers/users.controller')
 const router = Router();
 
 
 router.get('/', getUsers);
+router.post('/', createUser);
 
 module.exports = router;
