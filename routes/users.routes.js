77f32f82673f7b17127bbceca70d9ae3a24014ca
Chris-AM <c.aranguizm@outlook.com>
2021-09-13 22:57:21 -0300

    consuming updateUser method from controller and check from express-validator to  validate that required fields are not empty

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,21 +2,39 @@
  * Route: /api/users
  */
 
-const {Router} = require('express');
-const { check } = require('express-validator')
-const {getUsers, createUser} = require('../controllers/users.controller');
-const {validateFields} = require('../middlewares/validate-flieds');
+const { Router } = require("express");
+const { check } = require("express-validator");
+const {
+  getUsers,
+  createUser,
+  updateUser,
+} = require("../controllers/users.controller");
+const { validateFields } = require("../middlewares/validate-flieds");
 
 const router = Router();
 
-
-router.get('/', getUsers);
-router.post('/', [
-    check('name','name is required').not().isEmpty(),
-    check('lastName','last name is required').not().isEmpty(),
-    check('email','email is required').isEmail(),
-    check('password','password is required').not().isEmpty(),
-    validateFields
-], createUser);
-
+router.get("/", getUsers);
+router.post(
+  "/",
+  [
+    //middleware which validates that all required fields aren't empty
+    check("name", "name is required").not().isEmpty(),
+    check("lastName", "last name is required").not().isEmpty(),
+    check("email", "email is required").isEmail(),
+    check("password", "password is required").not().isEmpty(),
+    validateFields,
+  ],
+  createUser
+  );
+  router.put(
+    "/:id",
+    [
+    //middleware which validates that all required fields aren't empty
+    check("name", "name is required").not().isEmpty(),
+    check("lastName", "last name is required").not().isEmpty(),
+    check("email", "email is required").isEmail(),
+    check("role", "role is required").not().isEmpty(),
+  ],
+  updateUser
+);
 module.exports = router;
