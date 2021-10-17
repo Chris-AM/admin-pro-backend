da38e28738f08703c99d3f5b7a2a5abea211a3c3
chris.am <c.aranguizm@outlook.com>
2021-10-17 01:35:09 -0300

    creating and getting hospitals

--- controllers/hospitals.controller.js
+++ controllers/hospitals.controller.js
@@ -1,27 +1,63 @@
 const { response, request } = require("express");
 const bcrypt = require("bcryptjs");
-const User = require("../models/user.model");
+const Hospital = require("../models/hospital.model");
 const { generateJWT } = require("../helpers/jwt")
 
 const getHospitals = async (req, res) => {
+    const hospitals = await Hospital.find({}, "name");
     res.json({
         ok: true,
-        msg: 'getHospital'
+        msg: 'getting hospitals',
+        hospitals
     })
 };
 
 const getHospitalById = async (req = request, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'getHospitalById'
-    })
+    const hospId = req.params.id;
+    try {
+        const hospital = await Hospital.findById(hospId);
+        if (!hospital) {
+            return res.status(404).json({
+                ok:false,
+                msg: 'Hospital id not found'
+            });
+        } else {
+            res.json({
+                ok: true,
+                msg: 'hospital found',
+                hospital
+            })
+        }
+    } catch (error) {
+        console.log('Error -->', error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Internal server error. Check logs'
+        });
+    }
 }
 
-const createHospital = async (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'createHospital'
-    })
+const createHospital = async (req = request, res = response) => {
+    const uid = req.uid;
+    const hospital = new Hospital({
+        user: uid,
+        ...req.body
+    });
+    console.log('USER ID -->', uid)
+    try {
+        const hospitalInDB = await hospital.save();
+        res.json({
+            ok: true,
+            msg: 'Hospital created',
+            hospital: hospitalInDB
+        });
+    } catch (error) {
+        console.log('error -->', error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Internal Server error. Check Logs'
+        });
+    }
 };
 
 const updateHospital = async (req = request, res = response) => {
