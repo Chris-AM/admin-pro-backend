e1495d8a867d47bbd25a8f53cb8edb45de14a18c
chris.am <c.aranguizm@outlook.com>
2021-10-18 17:42:07 -0300

    search data route added to index

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use('/api/login', require('./routes/auth.routes'));
 app.use('/api/users', require('./routes/users.routes'));
 app.use('/api/hospitals', require('./routes/hospitals.routes'));
 app.use('/api/doctors', require('./routes/doctors.routes'));
+app.use('/api/search', require('./routes/searches.routes'));
 
 //port
 app.listen(port, () => {
